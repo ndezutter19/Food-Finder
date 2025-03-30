2902e60f1345aa655833a06ff6049fb6986fafa8
Noah DeZutter <noahdezutter20@gmail.com>
2025-03-30 12:41:49 -0700

    Initial Commit

---
+++ FoodFinder/FoodFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFinderApp.swift
+//  FoodFinder
+//
+//  Created by Noah DeZutter on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
